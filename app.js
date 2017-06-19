e9a84713bacd0c9dff7e87b09358dfb5e00205ba
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-19 16:38:10 -0500

    constructor function, and method created to build survey item content

--- app.js
+++ app.js
@@ -8,5 +8,30 @@ function SurveyItem(name,price,description,imageFile) {
   this.numberOfClicks = 0;
   this.numberOfTimesShown = 0;
   this.lastLoopUsed = 0;
+  this.pairedWith = 0;
 
 }
+
+SurveyItem.prototype.buildSurveyItem = function(conatainerId){
+  var container = document.getElementById(conatainerId);
+  var newSurveyImageBox = document.createElement('div');
+  var newSurveyItemDesc = document.createElement('div');
+  var newImage = document.createElement('img');
+  var newItemName = document.createElement('h4');
+  var newItemDesc = document.createElement('p');
+  newSurveyImageBox.setAttribute('class','surveyImageBox');
+  newSurveyItemDesc.setAttribute('class','surveyItemDesc');
+  newImage.setAttribute('src',this.itemImageFile);
+  newImage.setAttribute('alt',this.itemName);
+  newItemName.innerText = this.itemName;
+  newItemDesc.innerText = this.itemDesc;
+  container.appendChild(newSurveyImageBox);
+  container.appendChild(newSurveyItemDesc);
+  newSurveyImageBox.appendChild(newImage);
+  newSurveyItemDesc.appendChild(newItemName);
+  newSurveyItemDesc.appendChild(newItemDesc);
+};
+
+var boots = new SurveyItem('Jabba','$19.50','Ubb maul mace gev jerjerrod dressellian. Falleen arkanis iridonian desann skywalker greedo priapulin hapes.','images/boots.jpg');
+
+boots.buildSurveyItem('surveyThirdItem');
