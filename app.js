7bfb58e4a2b484a6ace8b9aad64ab3810d9ab336
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-19 16:04:33 -0500

    constructor function created

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function SurveyItem(name,price,description,imageFile) {
+  this.itemName = name;
+  this.itemPrice = price;
+  this.itemDesc = description;
+  this.itemImageFile = imageFile;
+  this.numberOfClicks = 0;
+  this.numberOfTimesShown = 0;
+  this.lastLoopUsed = 0;
+
+}
