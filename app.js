e364305e54dd20c7fca64bb6a0a9244eb19e7bd8
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-20 01:17:05 -0500

    styling

--- app.js
+++ app.js
@@ -34,46 +34,48 @@ SurveyItem.prototype.buildSurveyItem = function(conatainerId){
 
 var boots = new SurveyItem('Jabba','$19.50','Ubb maul mace gev jerjerrod dressellian. Falleen arkanis iridonian desann skywalker greedo priapulin hapes.','images/boots.jpg');
 var chair = new SurveyItem('Leah','$99.99','Neimoidia bollux momaw droid saffa sing b4-d4 tierce. Saurin roan jarael kastolar nunb ralter.','images/chair.jpg');
-var scissors = new SurveyItem('Han','$7.89','Shistavanen phlog depa oppo qui-gon t88 dorvalla. Ailyn darth irek max jabba finis kiffar antonio bith.','images/schissors.jpg');
-var water_can = new SurveyItem('Boba','$10.00','Quence mirta kalee qui-gon halla seerdon bibble. Calamari jacen tund tierce hutt salacious alderaan.','images/water_can');
-var wine_glass = new SurveyItem('Vader','$23.99','Vader saffa arkanis kyle givin yavin. Utapaun selkath dat mccool zuggs medon derlin kathol secura.','images/wine_glass');
+var scissors = new SurveyItem('Han','$7.89','Shistavanen phlog depa oppo qui-gon t88 dorvalla. Ailyn darth irek max jabba finis kiffar antonio bith.','images/scissors.jpg');
+var water_can = new SurveyItem('Boba','$10.00','Quence mirta kalee qui-gon halla seerdon bibble. Calamari jacen tund tierce hutt salacious alderaan.','images/water_can.jpg');
+var wine_glass = new SurveyItem('Vader','$23.99','Vader saffa arkanis kyle givin yavin. Utapaun selkath dat mccool zuggs medon derlin kathol secura.','images/wine_glass.jpg');
 var boots2 = new SurveyItem('Jabba2','$19.50','Ubb maul mace gev jerjerrod dressellian. Falleen arkanis iridonian desann skywalker greedo priapulin hapes.','images/boots.jpg');
 var chair2 = new SurveyItem('Leah2','$99.99','Neimoidia bollux momaw droid saffa sing b4-d4 tierce. Saurin roan jarael kastolar nunb ralter.','images/chair.jpg');
-var scissors2 = new SurveyItem('Han2','$7.89','Shistavanen phlog depa oppo qui-gon t88 dorvalla. Ailyn darth irek max jabba finis kiffar antonio bith.','images/schissors.jpg');
-var water_can2 = new SurveyItem('Boba2','$10.00','Quence mirta kalee qui-gon halla seerdon bibble. Calamari jacen tund tierce hutt salacious alderaan.','images/water_can');
+var scissors2 = new SurveyItem('Han2','$7.89','Shistavanen phlog depa oppo qui-gon t88 dorvalla. Ailyn darth irek max jabba finis kiffar antonio bith.','images/scissors.jpg');
+var water_can2 = new SurveyItem('Boba2','$10.00','Quence mirta kalee qui-gon halla seerdon bibble. Calamari jacen tund tierce hutt salacious alderaan.','images/water_can.jpg');
 
 var itemObjects = [boots,chair,scissors,water_can,wine_glass];
+var itemsObjectsWorking = itemObjects;
 
 function getFirstRandom(){
-  var index = Math.floor(Math.random() * (itemsObjectsWorking.length + 1));
+  var index = Math.floor(Math.random() * (itemsObjectsWorking.length));
   itemsObjectsWorking[index].buildSurveyItem('surveyFirstItem');
   itemsObjectsWorking.splice(index,1);
 }
 
 function getSecondRandom(){
-  var index = Math.floor(Math.random() * (itemsObjectsWorking.length + 1));
+  var index = Math.floor(Math.random() * (itemsObjectsWorking.length));
   itemsObjectsWorking[index].buildSurveyItem('surveySecondItem');
   itemsObjectsWorking.splice(index,1);
 }
 
 function getThirdRandom(){
-  var index = Math.floor(Math.random() * (itemsObjectsWorking.length + 1));
+  var index = Math.floor(Math.random() * (itemsObjectsWorking.length));
   itemsObjectsWorking[index].buildSurveyItem('surveyThirdItem');
   itemsObjectsWorking.splice(index,1);
 }
 
 function getRandomItems(){
-  var itemsObjectsWorking = itemObjects;
   var indexesjToRemove = [];
   for(var i = 0; i < itemObjects.length; i++){
     if(itemObjects[i].lastLoopUsed){
       indexesjToRemove.push(i);
     }
   }
-  for(var j = 0; j < indexes.length; i++){
+  for(var j = 0; j < indexesjToRemove.length; i++){
     itemsObjectsWorking.splice(j,1);
   }
   getFirstRandom();
   getSecondRandom();
   getThirdRandom();
 }
+
+getRandomItems();
