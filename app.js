3f77b1b342ce75bf3bc4ef233980300820cd7283
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-19 22:13:27 -0500

    build each of 5 images

--- app.js
+++ app.js
@@ -33,9 +33,10 @@ SurveyItem.prototype.buildSurveyItem = function(conatainerId){
 };
 
 var boots = new SurveyItem('Jabba','$19.50','Ubb maul mace gev jerjerrod dressellian. Falleen arkanis iridonian desann skywalker greedo priapulin hapes.','images/boots.jpg');
-var aViolet = new SurveyItem('Jabba','$19.50','Ubb maul mace gev jerjerrod dressellian. Falleen arkanis iridonian desann skywalker greedo priapulin hapes.','images/african-violet-.png');
-var violeta = new SurveyItem('Jabba','$19.50','Ubb maul mace gev jerjerrod dressellian. Falleen arkanis iridonian desann skywalker greedo priapulin hapes.','images/violeta.png');
-
+var chair = new SurveyItem('Leah','$99.99','Neimoidia bollux momaw droid saffa sing b4-d4 tierce. Saurin roan jarael kastolar nunb ralter.','images/chair.jpg');
+var scissors = new SurveyItem('Han','$7.89','Shistavanen phlog depa oppo qui-gon t88 dorvalla. Ailyn darth irek max jabba finis kiffar antonio bith.','images/schissors.jpg');
+var water_can = new SurveyItem('Boba','$10.00','Quence mirta kalee qui-gon halla seerdon bibble. Calamari jacen tund tierce hutt salacious alderaan.','images/water_can');
+var wine_glass = new SurveyItem('Vader','$23.99','Vader saffa arkanis kyle givin yavin. Utapaun selkath dat mccool zuggs medon derlin kathol secura.','images/wine_glass');
 /*
 boots.buildSurveyItem('surveyFirstItem');
 aViolet.buildSurveyItem('surveySecondItem');
